78c703f00f0f5b777fdb85089618a2485b3f638e
yann112 <yann_maisonneuve@yahoo.fr>
2025-04-21 00:36:06 +0200

    update webdrivers

--- sources/wgscraper/webdrivers.py
+++ sources/wgscraper/webdrivers.py
@@ -32,6 +32,7 @@ class InitWebDriver:
                     self.logger.info("Adding --headless=new argument to ChromeOptions.")
                     chrome_options.add_argument("--headless=new")
                     self.logger.info("Adding comprehensive stabilization arguments to ChromeOptions.")
+                    chrome_options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
                     chrome_options.add_argument("--no-sandbox")
                     chrome_options.add_argument("--disable-dev-shm-usage")
                     chrome_options.add_argument("--disable-extensions")
